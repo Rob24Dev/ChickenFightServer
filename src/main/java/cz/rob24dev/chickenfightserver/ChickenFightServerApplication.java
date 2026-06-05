d00f7677440e2361a535f3d3075b0bccc85d5f45
Rob24Dev <darvasrobert65@gmail.com>
2026-06-05 18:14:27 +0200

    first commit

---
+++ src/main/java/cz/rob24dev/chickenfightserver/ChickenFightServerApplication.java
@@ -0,0 +1,13 @@
+package cz.rob24dev.chickenfightserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenFightServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChickenFightServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/rob24dev/chickenfightserver/ChickenFightServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.rob24dev.chickenfightserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenFightServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
